935fa24911a3e8a85b147eb7f13107c501612105
Oanh Le <hoichima33@gmail.com>
2024-03-22 10:35:04 +0700

    chuong trinh hoan chinh 1

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author hp
+ */
+import GUI.StoreScreen;
+public class Main {
+    public static void main(String []args){
+        StoreScreen s= new StoreScreen();
+    }
+}
